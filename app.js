bc2bd3135a44e185de756c8cd818a9026138be58
Bryan Yoelin <bryoelin@gmail.com>
2017-04-30 16:38:35 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bryan on 4/30/17.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
